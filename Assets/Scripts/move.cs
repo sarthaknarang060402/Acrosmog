170236164355d3f304638b42bf5fd4b6ae431a95
sarthaknarang060402 <sarthaknarang666666@gmail.com>
2022-03-01 18:10:47 +0530

    addingfiles

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class move : MonoBehaviour
+{
+    float speed=10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        speed=speed+0.003f;
+        transform.position += Camera.main.transform.forward * Time.deltaTime*speed;
+        if(Input.GetMouseButtonDown(0))SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
